78e1d2acc069956305ab1ce7123d2c3dce346240
Caio Santos <caiosantos.eng@hotmail.com>
2023-04-05 11:26:47 -0300

    Melhorando responsividade

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -1,18 +1,18 @@
-import {FaLinkedinIn, FaGithub, FaInstagram} from 'react-icons/fa'
+import {FaLinkedinIn, FaGithub, FaWhatsapp} from 'react-icons/fa'
 
 import '../styles/components/socialNetworks.sass'
 
 const socialNetworks = [
-    {name: "linkedin", icon: <FaLinkedinIn/>},
-    {name: "github", icon: <FaGithub/>},
-    {name: "instagram", icon: <FaInstagram/>}
+    {name: "linkedin", icon: <FaLinkedinIn/>, link: "https://www.linkedin.com/in/caio-santos-332889160/" },
+    {name: "github", icon: <FaGithub/>, link: "https://github.com/CaioMAS"},
+    {name: "whatsapp", icon: <FaWhatsapp/>, link: "https://whatsfacil.com/415a54"}
 ]
 
 const SocialNetworks = () => {
     return (
         <section id='social-networks'>
            {socialNetworks.map((network) => (
-            <a href="#" className='social-btn' id={network.name} key={network.name}>{network.icon}</a>
+            <a target='_blank' href={network.link} className='social-btn' id={network.name} key={network.name}>{network.icon}</a>
            ))}
         </section>
     )
